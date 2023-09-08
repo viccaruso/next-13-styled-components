a5e8d7e7ca285bdb63a86f2bd7beac5749dff3d7
Victor Caruso <vic.caruso@gmail.com>
2023-09-08 11:20:21 -0700

    Remove superfluous function declaration

--- src/components/MainWrapper/MainWrapper.js
+++ src/components/MainWrapper/MainWrapper.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledMain = styled.main`
+const MainWrapper = styled.main`
   width: 100%;
   max-width: 800px;
   padding: 16px 24px;
@@ -11,9 +11,5 @@ const StyledMain = styled.main`
   border-radius: 2px;
   background: white;
 `;
-
-function MainWrapper({ children }) {
-  return <StyledMain>{children}</StyledMain>;
-}
 
 export default MainWrapper;
